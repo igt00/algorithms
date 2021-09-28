fa62c200ed0d6cabf53a0412a872586539e19bb1
igt00 <chupilko.vika2015@gmail.com>
2021-09-28 20:10:39 +0300

    added print

--- base.py
+++ base.py
@@ -1,5 +1,5 @@
 from random import randint
-
+print('b')
 
 def get_list_of_randint(start_index: int = 0, end_index: int = 50, count: int = 20):
     """Получение списка случайных int чисел от start_index до end_index, элементов в списке - count"""
